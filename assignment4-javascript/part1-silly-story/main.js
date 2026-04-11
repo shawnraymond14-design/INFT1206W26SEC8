0f65f7beae8439bfa4f03e262bb49aed729441a5
shawnraymond14-design <shawn.raymond14@gmail.com>
2026-04-10 20:17:35 -0400

    Add initial HTML and JavaScript files

---
+++ assignment4-javascript/part1-silly-story/main.js
@@ -0,0 +1,45 @@
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+const storyText = "It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+
+  let newStory = storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replaceAll(":insertx:", xItem);
+  newStory = newStory.replaceAll(":inserty:", yItem);
+  newStory = newStory.replaceAll(":insertz:", zItem);
+
+  if (customName.value !== "") {
+    let name = customName.value;
+    newStory = newStory.replaceAll("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    let weight = Math.round(300 / 14) + " stone";
+    let temperature = Math.round((94 - 32) * 5 / 9) + " Celsius";
+
+    newStory = newStory.replaceAll("300 pounds", weight);
+    newStory = newStory.replaceAll("94 Fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
